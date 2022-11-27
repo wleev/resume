a35f4575fc83a4db4557f2416d2c1f272c778ed7
Wesley <wlee@isabot.net>
2022-11-27 16:41:56 +0900

    Initial commit

---
+++ pdf-print.js
@@ -0,0 +1,25 @@
+const puppeteer = require("puppeteer");
+const path = require("path");
+const fs = require("fs");
+
+const filePath = path.resolve(__dirname, './out/resume.pdf.html');
+const cssPath = path.resolve(__dirname, './out/positive-pdf.css');
+(async () => {
+    const htmlContent = fs.readFileSync(filePath);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(htmlContent.toString(), { waitUntil: ["domcontentloaded", "networkidle0"]})
+    await page.pdf({
+        path: "out/resume.pdf",
+        format: "A4",
+        printBackground: true,
+        displayHeaderFooter: false,
+        margin: {
+            top: "10mm",
+            left: "10mm",
+            right: "10mm",
+            bottom: "10mm"
+        }
+    });
+    await browser.close();
+})();
