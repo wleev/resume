be4b6ce05ce074a73ea700fdfa9554481e78dd14
wleev <wlee@isabot.net>
2025-10-06 18:07:07 +0900

    Add some extra font logic for japanese compat

--- pdf-print.js
+++ pdf-print.js
@@ -9,9 +9,16 @@ const cssPath = path.resolve(__dirname, './out/positive-pdf.css');
     const htmlContent = fs.readFileSync(filePath, { encoding: "utf8" });
     const htmlContentJP = fs.readFileSync(filePathJP, { encoding: "utf8" });
     const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'] // Disable sandboxing
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--font-render-hinting=none',
+            '--disable-font-subpixel-positioning'
+        ]
     });
     const page = await browser.newPage();
+    
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36');
     
     await page.setContent(htmlContent, { waitUntil: ["domcontentloaded", "networkidle0"]})
     await page.pdf({
@@ -27,12 +34,14 @@ const cssPath = path.resolve(__dirname, './out/positive-pdf.css');
         }
     });
     
-    await page.setContent(htmlContentJP, { waitUntil: ["domcontentloaded", "networkidle0"]})
+    await page.setContent(htmlContentJP, { waitUntil: ["domcontentloaded", "networkidle0"]});
+    
     await page.pdf({
         path: "out-ja/resume-ja.pdf",
         format: "A4",
         printBackground: true,
         displayHeaderFooter: false,
+        waitForFonts: true,
         margin: {
             top: "10mm",
             left: "10mm",
